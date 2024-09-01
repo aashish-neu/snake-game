3171cb61d5c6eadbbc0bf62eb1ec60bcf8f66b98
Aashish Neupane <165367795+aashish-neu@users.noreply.github.com>
2024-09-01 15:00:13 +0545

    Update a star little

--- astar.js
+++ astar.js
@@ -1,5 +1,7 @@
 // defining heuristics valeus
 
+import { getSnakeHead } from "./snake";
+
 let distCurrentToNextNode = 0; //dist between current position in grid to next grid position
 let distCurrentToFinalNode = 0; //dist between current position in grid to food (direciton)
 
@@ -15,12 +17,17 @@ let totalHeuristicDistance = 0; //sum of above two
     x. Create g(distCurrentToNextNode) and h(distCurrentToFinalNode) and {f = g + h} (totalHeuristicDistance)
     x. Create open list (store nodes whose f cost already calculated) and closed list (store nodes whose neighbors have already been evaluated)
     x. Add start node (snake head) to open list
-    x. Create a infinite loop
+    x. Create an infinite loop
     x. Create temporary variable called 'currentNode' to store lowest f cost node (at start, it will be snake head).
     x. Remove currentNode from open list and add it to closed list
     x. Check if currentNode is equal to food position
             x. if equal break out of loop
     x. For each neighbor nodes of currentNode, if in closed list ignore and go to next neighbour node
+    x. If new path to neighbour is shorter (g - distCurrentToNextNode or f cost???) OR neighbour is not in OPEN list:
+            x. set fCost of neighbour node
+            x. change currentNode = parent of neighbour node
+            x. If neighbour is not in open list:
+                    x. Add neighbour node to open list
     x. 
     x. If two or more nodes with lowest f cost, then
             choose node with lowest h cost (distCurrentToFinalNode)
@@ -28,6 +35,9 @@ let totalHeuristicDistance = 0; //sum of above two
 
 
 */
+
+let openList = [];
+let closedList = [];
 
 let euclideanDist = 0;
 
@@ -37,7 +47,6 @@ function calculateEucDist(x0, y0, x1, y1) {
 }
 
 // find manhattan distance
-
 // move in eight direction (not diagonal)
 
 let manhattanDist = 0;
@@ -46,3 +55,44 @@ function calculateManhatDist(x0, y0, x1, y1) {
     manhattanDist = Math.abs(x0 - x1) + Math.abs(y0 - y1);
     return manhattanDist;
 }
+
+// function to find smalles f cost from openList
+
+function getSmallestFCost() {
+    let smallestFCost = Math.min(...openList);
+    return smallestFCost;
+}
+
+// all functions to calculate heuristics
+// function to find h cost (distCurrentToFinalNode)
+
+function getHCost(currentNode, food) {
+    return calculateEucDist(currentNode.x, currentNode.y, food.x, food.y);
+}
+
+// learn how to find g cost
+// for now
+function getGCost(currentNode, successorNode) {
+    // get successorNode
+    return calculateEucDist(currentNode.x, currentNode.y, successorNode.x, successorNode.y);
+    // OR to get successorNode; get whole open list and find successorNode by iterating through open list
+}
+
+function getFCost(currentNode) {
+    let currentFCost = getGCost() + getHCost();
+}
+
+// main loop
+function aiLoop() {
+    openList[0] = [getSnakeHead()];
+    // start of loop
+    while(true) {
+        let currentNode = getSmallestFCost();
+
+
+
+
+    }
+}
+
+
