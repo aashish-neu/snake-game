26291bdc711c84e76c0780ee1b165d33c18f7754
Aashish Neupane <165367795+aashish-neu@users.noreply.github.com>
2024-09-01 13:17:59 +0545

    Create unfinished algo for a star

--- astar.js
+++ astar.js
@@ -1,12 +1,33 @@
 // defining heuristics valeus
 
 let distCurrentToNextNode = 0; //dist between current position in grid to next grid position
-let distCurrentToFinalNode = 0; //dist between current position in grid to food
+let distCurrentToFinalNode = 0; //dist between current position in grid to food (direciton)
+
+let totalHeuristicDistance = 0; //sum of above two
 
 
 // find eucleadian distance
 
 // move in any direction
+
+// creating algorithm 
+/* 
+    x. Create g(distCurrentToNextNode) and h(distCurrentToFinalNode) and {f = g + h} (totalHeuristicDistance)
+    x. Create open list (store nodes whose f cost already calculated) and closed list (store nodes whose neighbors have already been evaluated)
+    x. Add start node (snake head) to open list
+    x. Create a infinite loop
+    x. Create temporary variable called 'currentNode' to store lowest f cost node (at start, it will be snake head).
+    x. Remove currentNode from open list and add it to closed list
+    x. Check if currentNode is equal to food position
+            x. if equal break out of loop
+    x. For each neighbor nodes of currentNode, if in closed list ignore and go to next neighbour node
+    x. 
+    x. If two or more nodes with lowest f cost, then
+            choose node with lowest h cost (distCurrentToFinalNode)
+    x. 
+
+
+*/
 
 let euclideanDist = 0;
 
