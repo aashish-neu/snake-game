db9f66d21b4230a77dca24f71986ad2593755330
Aashish Neupane <165367795+aashish-neu@users.noreply.github.com>
2024-09-01 11:54:48 +0545

    Add manhattan distance function

--- astar.js
+++ astar.js
@@ -1,8 +1,21 @@
 // find eucleadian distance
 
+// move in any direction
+
 let euclideanDist = 0;
 
-function calculateEucDis(x0, y0, x1, x2) {
-    euclideanDist = Math.sqrt(Math.pow(x0 - x1, 2) + Math.pow(y0 - y1, 2))
+function calculateEucDist(x0, y0, x1, y1) {
+    euclideanDist = Math.sqrt(Math.pow(x0 - x1, 2) + Math.pow(y0 - y1, 2));
+    return euclideanDist;
 }
 
+// find manhattan distance
+
+// move in eight direction (not diagonal)
+
+let manhattanDist = 0;
+
+function calculateManhatDist(x0, y0, x1, y1) {
+    manhattanDist = Math.abs(x0 - x1) + Math.abs(y0 - y1);
+    return manhattanDist;
+}
